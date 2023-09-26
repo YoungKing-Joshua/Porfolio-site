10eada5586578064e013017e6d62587352a28042
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-09-26 23:52:32 +0000

    in progress

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,17 +1,24 @@
 import "./topbar.scss";
+// import {Person, Mail} from "@matarial-ui";
 
 export default function Topbar() {
 	return (
 		<div className='topbar' id='topbar'>
 			<div className='wrapper'>
 				<div className='left'>
-					<a href='#intro'>youngKing</a>
-					
+					<a href='#intro' className='logo'>
+						genius.
+					</a>
 				</div>
-                <h1>Hello</h1>
-				<div className='right'>
-                    Go Beyond
-                </div>
+				<div className='itemContainer'>
+					<Person className='icon' />
+					<span>(+233)54 648 7271</span>
+				</div>
+				<div className='itemContainer'>
+					<Mail className="icon"/>
+					<span>joshuadeialorse@gmail.com</span>
+				</div>
+				<div className='right'>Go Beyond</div>
 			</div>
 		</div>
 	);
