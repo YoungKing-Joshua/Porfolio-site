8f9b96b896ce741a0e5c635db8fa3d9be1e9bc9c
Joshua DeiAlorse <joshuadeialorse@gmail.com>
2023-09-28 13:10:32 +0000

    in progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import "./app.scss";
 import Topbar from "./components/topbar/Topbar";
-import Intro from "./components/intro/intro";
 import Porfolio from "./components/portfolio/Portfolio";
 import Contact from "./components/contacts/Contacts";
 import Testimonials from "./components/testimonials/Testimonials";
 import Works from "./components/works/works";
 import Menu from "./components/menu/Menu";
+import Intro from "./components/intro/intro";
 
 function App() {
-  const  [menuOpen, setMenuOpen] = useState(false);
+	const [menuOpen, setMenuOpen] = useState(false);
 
 	return (
 		<div className='app'>
-			<Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-			<Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
-      <div className='sections'>
+			<Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+			<Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+			<div className='sections'>
 				<Intro />
 				<Porfolio />
 				<Works />
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useRef } from "react";
 import "./intro.scss";
+import { init } from "ityped";
 
-export default function Intro(){
-    return(
-        <div className="intro" id="intro">
-         
-        </div>
-    )
+export default function Intro() {
+	const textRef = useRef();
+
+	useEffect(() => {
+		const myElement = () => {
+			init(textRef.current, {
+				showCursor: false,
+				backDelay: 1500,
+				backSpeed: 60,
+				strings: ["Software Engineer", "Fullstack Developer", "Content Creator"],
+			});
+		};
+
+		return () => myElement();
+	}, []);
+
+	return (
+		<div className='intro' id='intro'>
+			<div className='left'>
+				<div className='imgContainer'>
+					<img src='assets/man.png' alt='' />
+				</div>
+			</div>
+			<div className='right'>
+				<div className='wrapper'>
+					<h2>Hi There, I'm</h2>
+					<h1>Joshua Dei-Alorse</h1>
+					<h3>
+						Freelance <span ref={textRef}></span>
+					</h3>
+				</div>
+				<a href='#portfolio'>
+					<img src='assets/down.png' alt='' />
+				</a>
+			</div>
+		</div>
+	);
 }
